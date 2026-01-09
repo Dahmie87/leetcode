8cf253734d1087e482499c64c7c26d430bf00b0f
dahmie <omotayodamilare07@gmail.com>
2026-01-09 19:23:09 +0000

    add substring

---
+++ substring.py
@@ -0,0 +1,16 @@
+# input_string = "abccdd"
+# # repeat for length of string
+# i = 0
+# while i < len(input_string):
+
+#     str = ""
+#     print(str)
+
+# # check each char in the str
+# # for each new char add 1
+# # if next char is seen before terminate and
+#     # start 4 again
+
+array1 = [1, 2, 4, 6, 7]
+array1.insert(2, 0)
+print(array1)
