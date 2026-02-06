58651792a4b3cfbd0041273683bbbca071a62939
dahmie <omotayodamilare07@gmail.com>
2026-02-06 19:12:52 +0100

    ipdtae two sums

--- two_sums.py
+++ two_sums.py
@@ -1,5 +1,8 @@
+
 def two_sums(target, target_list):
+    # create hashmap storing each num as key and index as value
     num_to_index = {}
+
     for i, num in enumerate(target_list):
         compliment = target - num
         if compliment in num_to_index:
