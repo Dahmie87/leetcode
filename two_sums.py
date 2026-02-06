ebca7fc0d6fdd8cb107fa3f11e2209f2604f5934
dahmie <omotayodamilare07@gmail.com>
2026-02-06 19:11:35 +0100

    add two sums

---
+++ two_sums.py
@@ -0,0 +1,11 @@
+def two_sums(target, target_list):
+    num_to_index = {}
+    for i, num in enumerate(target_list):
+        compliment = target - num
+        if compliment in num_to_index:
+            return num_to_index[compliment], i
+        num_to_index[num] = i
+
+
+print(two_sums(3, [1, 4, 2])
+      )
