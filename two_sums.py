a9ab89b6e8995c76cb8f219047bd2dff379526cb
dahmie <omotayodamilare07@gmail.com>
2026-02-06 19:19:46 +0100

    update two sums 3

--- two_sums.py
+++ two_sums.py
@@ -6,11 +6,11 @@ def two_sums(target, target_list):
     for i, num in enumerate(target_list):
         # calculate the compliment on each element
         compliment = target - num
-
+        # check if compliment already exists in our map
         if compliment in num_to_index:
             return num_to_index[compliment], i
+        # else store the num at index i
         num_to_index[num] = i
 
 
-print(two_sums(3, [1, 4, 2])
-      )
+print(two_sums(3, [1, 4, 2]))
